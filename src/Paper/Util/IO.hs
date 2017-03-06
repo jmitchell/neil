2e6fb2a4ec9688b80cdf056ad08d67df0298edf9
Jacob Mitchell <jmitchell@member.fsf.org>
2017-03-06 10:49:15 -0800

    Report missing files.

--- src/Paper/Util/IO.hs
+++ src/Paper/Util/IO.hs
@@ -1,9 +1,12 @@
 
 module Paper.Util.IO where
 
+import System.Directory
 import System.IO
 
 readFile' x = do
+    e <- doesFileExist x
+    putStr $ if not e then "Missing file: " ++ show x else ""
     h <- openFile x ReadMode
     s <- hGetContents h
     seq (length s) $ do
